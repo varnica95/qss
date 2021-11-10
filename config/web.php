8661607f0b46955b3b3e3bfb6a083f1635bd75ff
Marin <marinvarnica@gmail.com>
2021-11-10 14:28:29 +0100

    config update

--- config/web.php
+++ config/web.php
@@ -1,7 +1,9 @@
 <?php
 
 $app->get("/login", [Qss\Http\Controllers\LoginController::class, "index"]);
+$app->get("/logout", [Qss\Http\Controllers\LoginController::class, "logout"]);
 $app->post("/login/store-token", [Qss\Http\Controllers\LoginController::class, "login"]);
-$app->get("/home", [Qss\Http\Controllers\HomeController::class, "index"]);
+
+$app->get("/", [Qss\Http\Controllers\HomeController::class, "index"])->middleware(true, true, "/");
 
 return $app;
